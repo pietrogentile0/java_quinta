ceb62ea65eae186abc176e7caecb715ce1d0312d
pietrogentile0 <pietrogentile03@gmail.com>
2021-12-12 21:18:15 +0100

    non funziona con richiesta con body

--- 211205_socket/app.java
+++ 211205_socket/app.java
@@ -1,6 +1,7 @@
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
@@ -14,27 +15,38 @@ public class app {
 
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            String req;
+            String req = "";
+
+            // works without body
+            // while ((req = in.readLine()).length() != 0) {
+            // System.out.println(req);
+            // }
+
+            // have to work if there's a body
             boolean isOneHead = false;
-            while(true){
-                req = in.readLine()
-                if(isOneHead && req.length() == 0){break;}
-                isOneHead = req.length() == 0 ?  true : false;
+            while ((req = in.readLine()) != "" || !isOneHead) { // the order is important
                 System.out.println(req);
+                isOneHead = (req == "") ? true : false;
             }
 
-            PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-            String res = "Ciao";
+            FileReader reader = new FileReader("htmlResponse.html");
+            BufferedReader br = new BufferedReader(reader);
+            String fromFile, res = "";
 
-            System.out.println("fine");
+            while ((fromFile = br.readLine()) != null) {
+                res = res + fromFile + "\n";
+            }
+            reader.close();
+            br.close();
+
+            PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
 
             out.println("HTTP/1.1 200 OK");
-            out.println("Content-Type: text/plain");
-            out.println("Content-Length:"+res.length()+"\r\n");
-            out.println("Ciao");
+            out.println("Content-Type: text/html");
+            out.println("Content-Length:" + res.length() + "\n");
+            out.print(res);
 
             out.close();
-            socket.close();
             serverSocket.close();
         } catch (Exception e) {
             System.out.println(e);
