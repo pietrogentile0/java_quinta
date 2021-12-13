1f868149888e28c53375f74d7615e961dd811f6e
pietrogentile0 <pietrogentile03@gmail.com>
2021-12-13 12:43:32 +0100

    da fare lettura del body

--- 211205_socket/app.java
+++ 211205_socket/app.java
@@ -2,9 +2,11 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.StringReader;
 
 public class app {
     public static void main(String[] args) {
@@ -14,20 +16,8 @@ public class app {
             Socket socket = serverSocket.accept(); // accept() returns a socket
 
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-            String req = "";
-
-            // works without body
-            // while ((req = in.readLine()).length() != 0) {
-            // System.out.println(req);
-            // }
-
-            // have to work if there's a body
-            boolean isOneHead = false;
-            while ((req = in.readLine()) != "" || !isOneHead) { // the order is important
-                System.out.println(req);
-                isOneHead = (req == "") ? true : false;
-            }
+            String req = readWithBody(in);
+            System.out.print(req);
 
             FileReader reader = new FileReader("htmlResponse.html");
             BufferedReader br = new BufferedReader(reader);
@@ -51,5 +41,38 @@ public class app {
         } catch (Exception e) {
             System.out.println(e);
         }
+    }
+
+    public static String readWithoutBody(BufferedReader in) throws IOException {
+        String req = "", temp = "";
+        while ((temp = in.readLine()) != null) {
+            req = req + temp + "\n";
+        }
+        return req;
+    }
+
+    public static String readWithBody(BufferedReader in) throws IOException {
+        String req = readWithoutBody(in), method = "";
+        char tempMethod;
+
+        // riconosce il metodo
+        // int i = 0;
+        // while ((tempMethod = req.charAt(i)) != ' ') {
+        // method = method + tempMethod;
+        // i++;
+        // }
+
+        // cerca il content length
+        // if (method != "GET") {
+        // BufferedReader reader = new BufferedReader(new StringReader(req));
+        // String reqLine = reader.readLine();
+        // char tempHeader;
+
+        // i = 0;
+        // while ((tempHeader = reqLine.charAt(i)) != ':') {
+
+        // }
+        // }
+        return req;
     }
 }
